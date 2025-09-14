ad6c3e21858e7731a8df943092426f6ab007cde0
gru <gru3530@git.co.kr>
2025-09-14 12:15:18 +0900

    관리자 권한으로 자동 실행

--- src/Program.cs
+++ src/Program.cs
@@ -1,33 +1,37 @@
+using System.Diagnostics;
 using System.Security.Principal;
 
 namespace MOGI
 {
 	internal static class Program
 	{
-		/// <summary>
-		///  The main entry point for the application.
-		/// </summary>
 		[STAThread]
 		static void Main()
 		{
 			if (!IsAdministrator())
 			{
-				DialogResult result = MessageBox.Show(
-					"이 프로그램은 일부 기능 사용을 위해 관리자 권한이 필요할 수 있습니다.\n\n권한 없이 계속 진행할 경우 예기치 않은 오류가 발생할 수 있습니다.\n\n계속하시겠습니까?",
-					"권한 확인",
-					MessageBoxButtons.OKCancel,
-					MessageBoxIcon.Warning
-				);
-
-				if (result == DialogResult.Cancel)
+				try
 				{
-					Application.Exit(); 
-					return; 
+					var startInfo = new ProcessStartInfo
+					{
+						UseShellExecute = true,
+						WorkingDirectory = Environment.CurrentDirectory,
+						FileName = Application.ExecutablePath,
+						Verb = "runas"
+					};
+					Process.Start(startInfo);
 				}
+				catch (System.ComponentModel.Win32Exception)
+				{
+					return;
+				}
+				Application.Exit();
+				return;
 			}
 
+			var config = ConfigManager.Instance;
+
 			Task_Manager.Instance.InitializeAndStartServices();
-			ApplicationConfiguration.Initialize();
 			Application.Run(new Form_Main());
 
 			Task_Manager.Instance.ShutdownServices();
@@ -35,9 +39,11 @@ namespace MOGI
 
 		static bool IsAdministrator()
 		{
-			WindowsIdentity identity = WindowsIdentity.GetCurrent();
-			WindowsPrincipal principal = new WindowsPrincipal(identity);
-			return principal.IsInRole(WindowsBuiltInRole.Administrator);
+			using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+			{
+				WindowsPrincipal principal = new WindowsPrincipal(identity);
+				return principal.IsInRole(WindowsBuiltInRole.Administrator);
+			}
 		}
 	}
 }
